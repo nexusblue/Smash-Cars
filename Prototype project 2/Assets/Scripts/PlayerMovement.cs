2a27197db1dd57ac950f4c4a9fb98de93dc1b297
Henry Gammad <Henryj4796@yahoo.com>
2019-02-11 13:46:43 -0600

    Altered main scene and henry scene

--- Prototype project 2/Assets/Scripts/PlayerMovement.cs
+++ Prototype project 2/Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour {
     public float spdMultPos = 1.5f;
     public float spdMultNeg = .75f;
     public float rotateSpeed = 15;
+    
 
 	// Use this for initialization
 	void Start () {
@@ -19,34 +20,36 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
+        CheckWASDInput();
 
-        if (Input.GetKey(KeyCode.A)){
+    }
+
+
+    private void CheckWASDInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             //transform.Rotate(0,0,rotateSpeed * Time.deltaTime) ;
             transform.Rotate(Vector3.down, rotateSpeed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.D) ) {
-            //transform.Rotate(0,0,rotateSpeed * Time.deltaTime) ;
+        if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
         }
         //Test for speed booster
-        if (Input.GetKey(KeyCode.W)){
-            //transform.Rotate(0,0,rotateSpeed * Time.deltaTime) ;
+        if (Input.GetKey(KeyCode.W))
+        {
             PlayerRb.velocity = transform.forward * speed * spdMultPos;
         }
         else if (Input.GetKey(KeyCode.S))
         {
-            //transform.Rotate(0,0,rotateSpeed * Time.deltaTime) ;
             PlayerRb.velocity = transform.forward * speed * spdMultNeg;
         }
         else
         {
-            //transform.Rotate(0,0,rotateSpeed * Time.deltaTime) ;
             PlayerRb.velocity = transform.forward * speed;
         }
-        Debug.Log("Speed is " + PlayerRb.velocity);
-
-
-
     }
 }
