034047a7ca9ed8f170c40823c8c14dbf6e296850
Steven Ly <ly.steven16@gmail.com>
2019-02-11 12:19:57 -0600

    Started working on health scripts

---
+++ Prototype project 2/Assets/Scripts/Attack.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour {
+
+    public int health = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other) {
+         if (other.tag == "Defense")
+        {
+            if (health < 100)
+            {
+                health = health + 10;
+                if (health > 100)
+                {
+                    health = 100;
+                }
+
+            }
+            if (other.tag == "Attack")
+            {
+                if (health < 100)
+                {
+                    health = health + 10;
+                    if (health > 100)
+                    {
+                        health = 100;
+                    }
+                }
+            }
+        }
+    }
+}
