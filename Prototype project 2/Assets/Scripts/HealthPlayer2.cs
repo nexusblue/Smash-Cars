5cb2e5ff898d8f2730fb6dcd190fc3d1617bdc34
Henry Gammad <Henryj4796@yahoo.com>
2019-02-13 20:30:54 -0600

    Adjusted fire rate and health

--- Prototype project 2/Assets/Scripts/HealthPlayer2.cs
+++ Prototype project 2/Assets/Scripts/HealthPlayer2.cs
@@ -1,21 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class HealthPlayer2 : MonoBehaviour
 {
-	private int health = 100; 
+	private int health = 100;
+
+
+    Text text;
+    public Transform P2HP;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        P2HP.GetComponent<Text>().text = "Player 2 Hp:" + health.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        P2HP.GetComponent<Text>().text = "Player 2 Hp:" + health.ToString();
     }
 
     private void OnTriggerEnter(Collider other)
