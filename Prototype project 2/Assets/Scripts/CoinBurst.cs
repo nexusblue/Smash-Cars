bb51ed59cf3bb30cb7a2ee9d72f308fb85ac977f
Henry Gammad <Henryj4796@yahoo.com>
2019-02-13 19:37:30 -0600

    Added in ui and altered coin

---
+++ Prototype project 2/Assets/Scripts/CoinBurst.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBurst : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((other.tag == "Player1") || (other.tag == "Player2")) {
+            Instantiate(Resources.Load("CoinEffect") , transform.position , transform.rotation);
+
+        }
+    }
+
+}
